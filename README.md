3a191971666b66365a7cac8a6b1e907960121b31
Henry Chen <henryc345@gmail.com>
2014-10-15 00:37:17 +0800

    dog

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 sketch-jquery
 =============
-
+A sketchpad made using jquery, no html, no css
 Learning jquery, javascript
--- sketch.js
+++ sketch.js
@@ -1,27 +1,8 @@
+//default number of squares
+var squaresPerRow = 50;
+
+//when the document is ready, create grid using Jquery
 $(document).ready(function() {
-  $('body').append('<div class="container"></div>')
-  for(var i = 0; i < 32; i++) {
-  $('.container').append('<ul></ul>');
-  }
-
-  for(var i = 0; i < 32; i++) {
-  $('ul').append('<li></li>');
-  }
-
-  $('li').css({
-    display:'inline-block',
-    height: '15px',
-    width: '15px',
-    border: '1px black solid',
-    margin: '0 1px',
-  }).on("mouseover", function() {
-    $(this).css("background-color", "blue");
-  });
-
-  $('ul').css({
-    'padding': '0',
-    'margin': '0'
-  });
-
 
 });
+
